2c50820aee615c22fbb8a643c72ba88b4304db18
Viveka Senthil <113158241+viveka-001@users.noreply.github.com>
2022-10-20 10:41:14 +0530

    Create RemoveDuplicate1.java

---
+++ RemoveDuplicate1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr=head;
+        if(head==null)return null;
+        if(head.next==null)return head;
+        while(curr.next!=null)
+        {
+            if(curr.val==curr.next.val)
+            {
+                curr.next=curr.next.next;
+            }
+            else
+            {
+                curr=curr.next;
+            }
+        }
+        return head;
+    }
+}
